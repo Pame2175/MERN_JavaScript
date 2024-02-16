0fe0977828f3a44f90e66b4a66629c6a35dea99a
Pame2175 <pamegonza.98.pg@gmail.com>
2024-02-16 15:36:33 -0300

    javascript spanish

---
+++ Predecir_salida/Problema_1.js
@@ -0,0 +1,6 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Sálida
+console.log(randomCar) // Salida: 'Tesla'
+console.log(otherRandomCar) // Salida: 'Mercedes'
